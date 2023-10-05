138ba44ef62c34a05c0fa84ea17b3342f6a40ee0
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2023-10-04 19:17:22 -0600

    Create Funciones.py

---
+++ Funciones.py
@@ -0,0 +1,11 @@
+# Funciones para calcular la capacitancia de distintos objetos
+
+
+def capacitanceParallel():
+    return 0
+
+def capacitanceSphere():
+    return 0
+
+def capacitanciaCylinder():
+    return 0
