72a31b54d291e5fdbb392b73ac37e9d365bd5bf4
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2023-10-05 18:25:59 -0600

    Nuevos cambios
    Falta ver lo de la densidad y verificar que toso esté bien con los cálculos

--- Functions.py
+++ Functions.py
@@ -7,48 +7,125 @@ e = 8.85418*(10**-12)
 #constante pi
 p = math.pi
 
-def capacitanceParallel(h, l, d , dielectric, quantity, n):
-    if(dielectric.lower() == "y" and quantity.lower() == "full"): 
-       return (k*e*h*l)/d
-    elif(dielectric.lower() == "y" and quantity.lower() == "half"):
-        return ((e*h*l)/2*d) + ((k*e*h*l)/2*d)
-    elif(dielectric.lower() == "n" and quantity == None):
-        return (e*h*l)/d
-    elif(n == 0):
-        return 0
+#arreglos para guardar las densidades de cargas
+freeCharges = [0,0,0,0]
+bondedCharges = [0,0,0,0]
+#para los emisferios de las cargas libres
 
-def capacitanceSphere(r_ext, r_int, dielectric, quantity, n):
-    if(dielectric.lower() == "y" and quantity.lower() == "full"): 
+def hasDielectric(dielectric):
+    if(dielectric == "y"):
+        quantity = str(input("full/half:" ))
+        return quantity
+    elif(dielectric == "n"):
+        quantity = None
+        return quantity
+        
+
+def capacitanceParallel(h, l, d):
+    return (e*h*l)/d
+
+def capacitanceSphere(r_ext, r_int):
+    return 4*p*e*(r_ext * r_int) / (r_ext - r_int)
+
+def capacitanceCylinder(r_ext, r_int, l):
+    return (2*p*e*l)/(math.log(r_ext) - math.log(r_int))
+
+def capacitanceParallelDielectric(h, l, d , dielectric, quantity):
+    if(dielectric == "y" and quantity == "full"): 
+       return (k*e*h*l)/d
+    elif(dielectric == "y" and quantity == "half"):
+        return ((e*h*l)/2*d) + ((k*e*h*l)/2*d)   
+    else:
+        return 0     
+
+def capacitanceSphereDielectric(r_ext, r_int, dielectric, quantity):
+    if(dielectric == "y" and quantity == "full"): 
         return 4*p*e*k*(r_ext * r_int) / (r_ext - r_int)
-    elif(dielectric.lower() == "y" and quantity.lower() == "half"):
+    elif(dielectric == "y" and quantity == "half"):
         return 2*p*e*(k+1)*(r_ext * r_int) / (r_ext - r_int)
-    elif(dielectric.lower() == "n" and quantity == None):
-        return 4*p*e*(r_ext * r_int) / (r_ext - r_int)
-    elif(n == 0):
+    else:
         return 0
     
-def capacitanceCylinder(r_ext, r_int, l, dielectric, quantity, n):
-    if(dielectric.lower() == "y" and quantity.lower() == "full"): 
+def capacitanceCylinderDielectric(r_ext, r_int, l, dielectric, quantity):
+    if(dielectric == "y" and quantity == "full"): 
         return (2*p*e*l*k)/(math.log(r_ext) - math.log(r_int))
-    elif(dielectric.lower() == "y" and quantity.lower() == "half"):
+    elif(dielectric == "y" and quantity == "half"):
         return (p*e*l)/(math.log(r_ext) - math.log(r_int)) + (p*e*l*k)/(math.log(r_ext) - math.log(r_int))
-    elif(dielectric.lower() == "n" and quantity == None):
-        return (2*p*e*l)/(math.log(r_ext) - math.log(r_int))
-    elif(n == 0):
+    else:
         return 0
-    
+#para calcular la carga
 def capacitanceCharge(C, V):
     return C * V
 
 #dependiendo si C_0 es 0 en caso tenga dielectrico, sino 
-def storedEnergy(C, C_0,  V):
-    return C*((V/k)**2)/2 + C_0*(V**2)/2
-  
+def storedEnergy(C, C_0,  V, quantity):
+    if(quantity == "half"):
+        return C*((V/k)**2)/2 + C_0*(V**2)/2
+    elif(quantity == "full"):
+        C_0 = 0
+        return C*((V/k)**2)/2 + C_0*(V**2)/2
+    else:
+        C = 0
+        return C*((V/k)**2)/2 + C_0*(V**2)/2
+# para la carga libre paralela
+def freeChargeParallel(Q, h, l, quantity):
+    if(quantity == "full"):
+        freeCharges[0] = (k*Q)/(h*l)
+        return freeCharges
+    elif(quantity == "half"):
+        #dos densidades
+        freeCharges[0] = (2*Q)/(h*l)
+        freeCharges[1] = 1
+        return freeCharges
 
-def freeCharge(Q, A):
-    return Q/A
+# para la carga ligada paralela
+def bondedChargeParallel(Q, h, l, quantity):
+    if(quantity == "full"):
+        bondedCharges[0] = (Q/(h*l)) * (1-(1/k))
+        return bondedCharges
+    elif(quantity == "half"):
+        bondedCharges[0] = (2*Q/(h*l)) 
+        return bondedCharges
 
-def bondedCharge(sigma):
-    return sigma*(1 - (1/k))
+# para la carga libre esferica
+def freeChargeSphere(Q, r_ext, r_int, quantity):
 
+    if(quantity == "full"):
+        #dos densidades de carga libre
+        freeCharges[0] = (k*Q)/(4*p*(r_int**2)*(1+k))
+        freeCharges[1] = (k*Q)/(4*p*(r_ext**2)*(1+k))
+        return freeCharges
+    elif(quantity == "half"):
+        #cuatro densidades de carga libre
+        freeCharges[0] = Q/(2*p*(r_int**2)*(1+k))
+        freeCharges[1] = Q/(2*p*(r_ext**2)*(1+k))
+        freeCharges[2] = (k*Q)/(2*p*(r_int**2)*(1+k))
+        freeCharges[3] = (k*Q)/(2*p*(r_ext**2)*(1+k))
+        return freeCharges
 
+# para la carga ligada esferica
+def bondedChargeSphere(Q, r_ext, r_int, quantity):
+    
+    if(quantity == "full"):
+        bondedCharges[0] = ((k-1)*Q) / (4*p*(r_int**2)*(1+k))
+        bondedCharges[1] = ((k-1)*Q) / (4*p*(r_ext**2)*(1+k))
+        return bondedCharges
+    elif(quantity == "half"):
+        
+        bondedCharges[0] =  ((k-1)*Q) / (2*p*(r_int**2)*(1+k))
+        bondedCharges[1] =  ((k-1)*Q) / (2*p*(r_ext**2)*(1+k))
+        return bondedCharges
+
+# para la carga libre cilindrica
+def freeChargeCylinder(quantity):
+    if(quantity == "full"):
+        return freeCharges
+    elif(quantity == "half"):
+        return freeCharges
+
+# para la carga ligada cilindrica
+def bondedChargeCylinder(quantity):
+    if(quantity == "full"):
+        return bondedCharges
+    elif(quantity == "half"):
+        return bondedCharges
--- Principal.py
+++ Principal.py
@@ -6,32 +6,32 @@ import Functions
 print("Ingrese qué tipo de capacitor es")
 print("1. Placas paralelas \n2. Esfera \n3. Cilindro")
 option = int(input())
-dielectric = str(input("¿Contiene dielectrico? y/n: "))
-if(dielectric.lower() == "y"):
-    quantity = str(input("full/half:" ))
-    n = 1
-elif(dielectric.lower() == "n"):
-    quantity = None
-    n = 0
+dielectric = str(input("¿Contiene dielectrico? y/n: ")).lower()
+quantity = Functions.hasDielectric(dielectric)
 #para las placas paralelas
 if(option == 1):
     h = float(input("Ingrese altura (m): "))
     l = float(input("Ingrese largo (m): "))
     d = float(input("Ingrese la distancia entre placas (m): "))
     v = float(input("Ingrese voltaje: "))
-    C_0 = Functions.capacitanceParallel(h, l, d, dielectric, quantity, n)
-    C = Functions.capacitanceParallel(h, l, d, dielectric, quantity, n)
+    C_0 = Functions.capacitanceParallel(h, l, d)
+    C = Functions.capacitanceParallelDielectric(h, l, d, dielectric, quantity)
     Q = Functions.capacitanceCharge(C, v)
-    U = Functions.storedEnergy(C, C_0, v)
-    print("C: ", C, "F")
-    print("Q: ", Q , "C")
-    print("U: ", U, "J")
-    if(quantity.lower() == "y"):
-        sigma = Functions.freeCharge(Q, v)
-        sigma_i = Functions.bondedCharge(sigma)
-        print("σ: ", sigma, "C")
-        print("σi: ", sigma_i, "C")
+    Q_0 = Functions.capacitanceCharge(C, v)
+    U = Functions.storedEnergy(C, C_0, v, quantity)
     
+    if(dielectric == "y"):
+        sigma = Functions.freeChargeParallel(Q, h, l, quantity)
+        sigma_i = Functions.bondedChargeParallel(Q, h,l,quantity)
+        print("C: ", C, "F")
+        print("Q: ", Q , "C")
+        print("U: ", U, "J")
+        print("σ: ", sigma, "C/m^2")
+        print("σi: ", sigma_i, "C/m^2")
+    else:
+        print("C: ", C_0, "F")
+        print("Q: ", Q_0 , "C")
+        print("U: ", U, "J")
 
 #para la esfera
 elif(option == 2):
@@ -39,16 +39,26 @@ elif(option == 2):
     r_int = float(input("Ingrese el radio interior (m): "))
     v = float(input("Ingrese voltaje: "))
     
-    C_0 = Functions.capacitanceSphere(r_ext, r_int, dielectric, quantity, n)
-    C = Functions.capacitanceSphere(r_ext, r_int, dielectric, quantity, n)
+    C_0 = Functions.capacitanceSphere(r_ext, r_int)
+    C = Functions.capacitanceSphereDielectric(r_ext, r_int, dielectric, quantity)
     Q = Functions.capacitanceCharge(C, v)
-    U = Functions.storedEnergy(C, C_0, v)
-    print("C: ", C, "F")
-    print("Q: ", Q , "C")
-    print("U: ", U, "J")
-    if(quantity.lower() == "y"):
-        print("σ: ", "C")
-        print("σi: ", "C")
+    Q_0 = Functions.capacitanceCharge(C_0, v)
+    U = Functions.storedEnergy(C, C_0, v, quantity)
+    
+    if(dielectric == "y"):
+        sigma = Functions.freeChargeSphere(Q, r_ext, r_int, quantity)
+        sigma_i = Functions.bondedChargeSphere(Q, r_ext, r_int, quantity)
+        print("C: ", C, "F")
+        print("Q: ", Q , "C")
+        print("U: ", U, "J")
+        print("σ: ",sigma, "C/m^2")
+        print("σi: ", sigma_i, "C/m^2")
+    else:
+        print("C: ", C_0, "F")
+        print("Q: ", Q_0 , "C")
+        print("U: ", U, "J")
+        
+    
     
 
 #para el cilindro
@@ -58,14 +68,22 @@ elif(option == 3):
     l = float(input("Ingrese el largo (m): "))
     v = float(input("Ingrese voltaje: "))
     
-    C_0 = Functions.capacitanceCylinder(r_ext, r_int, l, dielectric, quantity, n)
-    C = Functions.capacitanceCylinder(r_ext, r_int, l, dielectric, quantity, n)
+    C_0 = Functions.capacitanceCylinder(r_ext, r_int)
+    C = Functions.capacitanceCylinderDielectric(r_ext, r_int, l, dielectric, quantity)
     Q = Functions.capacitanceCharge(C, v)
-    U = Functions.storedEnergy(C, C_0, v)
-    print("C: ", C, "F")
-    print("Q: ", Q , "C")
-    print("U: ", U, "J")
-    if(quantity.lower() == "y"):
-        print("σ: ", "C")
-        print("σi: ", "C")
+    Q_0 = Functions.capacitanceCharge(C_0, v)
+    U = Functions.storedEnergy(C, C_0, v, quantity)
+    
+    if(dielectric == "y"):
+        sigma = Functions.freeChargeCylinder(r_ext, r_int, l, quantity)
+        sigma_i = Functions.bondedChargeCylinder(r_ext, r_int, l, quantity)
+        print("C: ", C, "F")
+        print("Q: ", Q , "C")
+        print("U: ", U, "J")
+        print("σ: ", sigma, "C/m^2")
+        print("σi: ", sigma_i, "C/m^2")
+    else:
+        print("C: ", C_0, "F")
+        print("Q: ", Q_0, "C")
+        print("U: ", U, "J")
     
